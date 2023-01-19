07a95753f7ad15978fb01893a52103b2fc95f7cf
DanMitozz <117995535+DanMitozz@users.noreply.github.com>
2023-01-19 11:21:03 +0300

    Add files via upload

---
+++ levi.pas
@@ -0,0 +1,15 @@
+﻿uses graphabc;
+procedure levy(x,y,a,r:real;k:integer); 
+begin
+if k>0 then 
+ begin
+  r:=r*sin(pi/4);
+  levy(x,y,a+pi/4,r,k-1);
+  levy(x+r*cos(a+pi/4),y-r*sin(a+pi/4),a-pi/4,r,k-1);
+ end 
+else
+line(trunc(x),trunc(y),trunc(x+r*cos(a)),trunc(y-r*sin(a))); 
+end; 
+begin
+levy(200,300,0,250,3); 
+end.
