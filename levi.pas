d0e57111ba66807276507498939189b69de6c51b
DanMitozz <117995535+DanMitozz@users.noreply.github.com>
2023-01-24 11:30:41 +0300

    Add files via upload

---
+++ levi.pas
@@ -0,0 +1,21 @@
+﻿uses graphabc;
+var rad :real;
+procedure levy(x,y,a,r:real;k:integer); 
+
+begin
+if k>0 then 
+ begin
+  r:=r*sin(pi/4);
+  levy(x,y,a+pi/4,r,k-1);
+  levy(x+r*cos(a+pi/4),y-r*sin(a+pi/4),a-pi/4,r,k-1);
+ end 
+else
+line(trunc(x),trunc(y),trunc(x+r*cos(a)),trunc(y-r*sin(a))); 
+end; 
+begin
+SetWindowSize(1800,900);
+write('Введите масштаб до 450');
+read(rad);
+//levy(200,300,0,rad,13);
+levy(750,450,0,rad,13);
+end.
